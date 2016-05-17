8b711a19a4ed9fa76d622b7de9cb225c214a8aff
Tyler Crosse <tylerscottcrosse@gmail.com>
2016-05-16 22:46:07 -0400

    Add a bunch of stuff, still trying to figure out how to link it

---
+++ shell/.bash_profile
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+##############################
+#  Colors
+##############################
+
+# generic colouriser
+GRC=`which grc`
+if [ "$TERM" != dumb ] && [ -n "$GRC" ]
+    then
+        alias colourify="$GRC -es --colour=auto"
+        alias configure='colourify ./configure'
+        for app in {diff,make,gcc,g++,ping,traceroute}; do
+            alias "$app"='colourify '$app
+    done
+fi
+
+# highlighting inside manpages and elsewhere
+export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
+export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
+export LESS_TERMCAP_me=$'\E[0m'           # end mode
+export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
+export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
+export LESS_TERMCAP_ue=$'\E[0m'           # end underline
+export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+##############################
+#  Bash Completion
+##############################
+
+# z beats cd most of the time.
+#   github.com/rupa/z
+source ~/dev/dotfiles/shell/z.sh
+
+# bash completion.
+if  which brew > /dev/null && [ -f "$(brew --prefix)/share/bash-completion/bash_completion" ]; then
+    source "$(brew --prefix)/share/bash-completion/bash_completion";
+elif [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion;
+fi;
+
+# homebrew completion
+if  which brew > /dev/null; then
+    source `brew --repository`/Library/Contributions/brew_bash_completion.sh
+fi;
+
+# hub completion
+if  which hub > /dev/null; then
+    source "$(brew --prefix)/etc/bash_completion.d/hub.bash_completion.sh";
+fi;
+
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
+
+# Enable tab completion for `g` by marking it as an alias for `git`
+if type __git_complete &> /dev/null; then
+    __git_complete g __git_main
+fi;
+
+##############################
+#  Environments & Settings
+##############################
+
+export GITHUB_USERNAME='tylercrosse'
+export PATH="/usr/local/bin:$PATH"
+export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/9.4/bin
+
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Devel
+source /usr/local/bin/virtualenvwrapper.sh
+
+# Setting PATH for Python 3.5
+# The orginal version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.5/bin:${PATH}"
+export PATH
+
+export NVM_DIR="/Users/tcrosse/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
---
+++ shell/aliases.sh
@@ -0,0 +1,171 @@
+############################################
+#  Navigation - Movement & Manipulation
+############################################
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias -- -="cd -"
+
+# cd into whatever is the forefront Finder window.
+function cdf() {  # short for cdfinder
+  cd "`osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)'`"
+}
+
+# Create a new directory and enter it
+function md() {
+	mkdir -p "$@" && cd "$@"
+}
+
+alias mv='mv -v'
+alias rm='rm -i -v'
+alias cp='cp -v'
+
+############################################
+#  Navigation - Search & Display
+############################################
+
+# `cat` with beautiful colors. requires: sudo easy_install -U Pygments
+alias c='pygmentize -O style=monokai -f console256 -g'
+
+# brew install the_silver_searcher
+alias ag='ag -f --hidden'
+
+alias tre="tree -CshDL 1" # -L one level deep
+alias tre2="tree -CshDL 2" # -L two levels deep
+alias tre3="tree -CshDL 3" # -L two levels deep
+alias tred="tree -CshDd" # -d directories only
+alias trea="tree -CshDaL 1" # -L one level deep, including '.' files
+alias trea2="tree -CshDaL 2" # -L two levels deep, including '.' files
+alias trea3="tree -CshDaL 3" # -L two levels deep, including '.' files
+alias treaa="tree -CshDa" # -a all including '.' files
+alias tree="tree -CshD" # Color, size, date
+alias treee="tree"
+
+# find shorthand
+function f() {
+	find . -name "$1" 2>&1 | grep -v 'Permission denied'
+}
+
+# use coreutils `ls` if possible…
+hash gls >/dev/null 2>&1 || alias gls="ls"
+
+# always use color, even when piping (to awk,grep,etc)
+if gls --color > /dev/null 2>&1; then colorflag="--color"; else colorflag="-G"; fi;
+export CLICOLOR_FORCE=1
+
+# ls options: A = include hidden (but not . or ..), F = put `/` after folders, h = byte unit suffixes
+alias ls='gls -AFh ${colorflag} --group-directories-first'
+alias lsd='ls -l | grep "^d"' # only directories
+alias lsl="ls -lhF" # long format, human readable, classify
+alias lss="ls -GhF" # no group, human readable, classify
+
+# List all files, long format, colorized, permissions in octal
+function la(){
+ 	ls -l  "$@" | awk '
+    {
+      k=0;
+      for (i=0;i<=8;i++)
+        k+=((substr($1,i+2,1)~/[rwx]/) *2^(8-i));
+      if (k)
+        printf("%0o ",k);
+      printf(" %9s  %3s %2s %5s  %6s  %s %s %s\n", $3, $6, $7, $8, $5, $9,$10, $11);
+    }'
+}
+
+##############################
+#  Git Aliases
+##############################
+
+alias g="git"
+alias ga="git add"
+alias gs="git status"
+alias gcm="git commit -m"
+alias gpo="git push origin"
+alias gpom="git push origin master"
+alias gphm="git push heroku master"
+alias go="git-open"
+
+alias gl="git log --all --oneline --graph --decorate"
+alias gpr="git log --pretty=format:'%Cblue%h%Creset %Cgreen%ad%Creset | %s%C(yellow)%d%Creset [%an]' --graph --date=short --decorate"
+
+# github-email user https://github.com/paulirish/github-email
+
+# git clone & then cd
+function gc {
+  reponame=${1##*/}
+  reponame=${reponame%.git}
+  git clone "$1" "$reponame";
+  cd "$reponame";
+}
+
+##############################
+#  Other Aliases
+##############################
+
+alias obash="atom ~/.bash_profile"
+alias rbash="source ~/.bash_profile"
+
+alias rspecc="rspec -c"
+alias rspecf="rspec -c -fd"
+
+alias chro="open -a 'Google Chrome'"
+alias a="atom"
+
+alias oops='$(thefuck $(fc -ln -1))'
+
+alias w='ansiweather'
+
+##############################
+#  Network Aliases
+##############################
+
+# Networking. IP address, dig, DNS
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias dig="dig +nocmd any +multiline +noall +answer"
+# wget sucks with certificates. Let's keep it simple.
+alias wget="curl -O"
+
+alias hs-o="http-server -o"
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+	local port="${1:-8000}"
+	open "http://localhost:${port}/" &
+	# statik is good because it won't expose hidden folders/files by default.
+	# npm install -g statik
+	statik --port "$port" .
+}
+
+# get gzipped size
+function gz() {
+	echo "orig size    (bytes): "
+	cat "$1" | wc -c
+	echo "gzipped size (bytes): "
+	gzip -c "$1" | wc -c
+}
+
+# whois a domain or a URL
+function whois() {
+	local domain=$(echo "$1" | awk -F/ '{print $3}') # get domain from URL
+	if [ -z $domain ] ; then
+		domain=$1
+	fi
+	echo "Getting whois record for: $domain …"
+
+	# avoid recursion
+	# this is the best whois server
+	# strip extra fluff
+	/usr/bin/whois -h whois.internic.net $domain | sed '/NOTICE:/q'
+}
+
+function localip(){
+	function _localip(){ echo "📶  "$(ipconfig getifaddr "$1"); }
+	export -f _localip
+	local purple="\x1B\[35m" reset="\x1B\[m"
+	networksetup -listallhardwareports | \
+		sed -r "s/Hardware Port: (.*)/${purple}\1${reset}/g" | \
+		sed -r "s/Device: (en.*)$/_localip \1/e" | \
+		sed -r "s/Ethernet Address:/📘 /g" | \
+		sed -r "s/(VLAN Configurations)|==*//g"
+}
